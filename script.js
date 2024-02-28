fd5410c8de6dbf3dc6e06eef21136bb7232da408
elzhanurazhizha <elzhanurazhizha@gmail.com>
2024-02-28 15:27:13 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,76 @@
+function insert(num){
+    var textarea = document.form.textarea;
+    var jumlah = textarea.value;
+    var operator = ['+','-','*','/','.','00','%'];
+
+    if (jumlah === '0' && num ==='-') {
+        textarea.value = num;
+        return;
+    }
+    
+    if (jumlah === '0' && operator.includes(num)) {
+        if (jumlah.length === 1 && num === '.') {
+            textarea.value = jumlah + num;
+        }else{
+            return;
+        }
+    }
+
+    if (jumlah === '0' && num === '0') {
+        if (jumlah.length === 1 && num === '0' ) {
+            return;
+        }else{
+            textarea.value = jumlah + num;
+        }
+    }else{
+        if (jumlah.length === 1 && jumlah === '0') {
+            textarea.value = num
+        }else{
+            textarea.value = jumlah + num;
+        }
+    }
+
+    if (operator.includes(num)) {
+        var las = jumlah.charAt(jumlah.length -1);
+        // alert(las);
+        if (operator.includes(las)) {
+           textarea.value = jumlah.slice(0,-1) + num; 
+        }else{
+            textarea.value = jumlah + num;
+        }
+    }
+}
+
+function clean(){
+    document.form.textarea.value = '0';
+}
+function del(){
+    var hasil = document.form.textarea.value;
+    // alert(hasil);
+
+    if (hasil.length === 1) {
+        document.form.textarea.value = '0';
+    }else{
+        document.form.textarea.value = hasil.substring(0,hasil.length -1);
+    }
+}
+function equal(){
+    var hasil = document.form.textarea.value;
+
+    var las = hasil.charAt(hasil.length -1);
+        // alert(las);
+    if (las.includes('%')) {
+        var nilai = hasil.substring(0,hasil.length -1);
+
+        document.form.textarea.value = (nilai/100);
+    }
+    else{
+        var infinity = eval(hasil);
+        if (infinity === Infinity) {
+            alert('Bilangan Apapun Yang Di bagi 0 Tidak Terdefinisi!!');
+            document.form.textarea.value = '0';
+        }else{
+            document.form.textarea.value = eval(hasil);
+        }
+    }
+}
